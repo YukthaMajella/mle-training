8b9cc80eca22690ea9d7bb7c82563d2879f26eda
YukthaMajella <majella.biju@tigeranalaytics.com>
2025-01-31 08:33:01 +0000

    Pushing the workflow with test to verify installation

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,11 +1,21 @@
 import subprocess
 import sys
+
 import pytest
 
 
 def test_package_installation():
-    result = subprocess.run([sys.executable, "-m", "pip", "install", "./dist/*.whl"], 
-                            capture_output=True, text=True)
+    result = subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "./dist/HousePricingPredictor-0.0.1-py3-none-any.whl",
+        ],
+        capture_output=True,
+        text=True,
+    )
 
     assert result.returncode == 0, f"Installation failed: {result.stderr}"
 
@@ -13,4 +23,3 @@ def test_package_installation():
         from HousePricingPredictor import nonstandardcode
     except ImportError as e:
         pytest.fail(f"Package import failed: {str(e)}")
-
