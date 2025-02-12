756a3477689047399f0bbe945971678073beaf06
YukthaMajella <majella.biju@tigeranalaytics.com>
2025-02-12 07:32:09 +0000

    Incorporating logs into the code

--- src/house_pricing_predictor/model_scoring.py
+++ src/house_pricing_predictor/model_scoring.py
@@ -1,14 +1,20 @@
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-
+import logging
+logger = logging.getLogger(__name__)
 
 def model_scoring(model, housing_prepared, housing_labels):
-    housing_predictions = model.predict(housing_prepared)
-    mse = mean_squared_error(housing_labels, housing_predictions)
-    rmse = np.sqrt(mse)
-    mae = mean_absolute_error(housing_labels, housing_predictions)
-    print("MSE:", mse)
-    print("RMSE:", rmse)
-    print("MAE:", mae)
+    try:
+        logger.info("Predicting for test data...")
+        housing_predictions = model.predict(housing_prepared)
+        mse = mean_squared_error(housing_labels, housing_predictions)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(housing_labels, housing_predictions)
+        print("MSE:", mse)
+        print("RMSE:", rmse)
+        print("MAE:", mae)
+        logger.debug("Model scored for test data successfully.")
+    except Exception as e:
+        logger.error(f"Error while scoring test data: {e}")
     return housing_predictions, mse, rmse, mae
 
--- src/house_pricing_predictor/model_training.py
+++ src/house_pricing_predictor/model_training.py
@@ -4,7 +4,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.tree import DecisionTreeRegressor
-
+import logging
+logger = logging.getLogger(__name__)
 
 def cv_results(model):
     cvres = model.cv_results_
@@ -13,48 +14,60 @@ def cv_results(model):
 
 
 def model_training(housing_prepared, housing_labels):
-    lin_reg = LinearRegression()
-    lin_reg.fit(housing_prepared, housing_labels)
 
-    tree_reg = DecisionTreeRegressor(random_state=42)
-    tree_reg.fit(housing_prepared, housing_labels)
+    try:
+        logger.info("Training the models...")
 
-    param_distribs = {
-        "n_estimators": randint(low=1, high=200),
-        "max_features": randint(low=1, high=8),
-    }
-    forest_reg = RandomForestRegressor(random_state=42)
-    rnd_search = RandomizedSearchCV(
-        forest_reg,
-        param_distributions=param_distribs,
-        n_iter=10,
-        cv=5,
-        scoring="neg_mean_squared_error",
-        random_state=42,
-    )
-    rnd_search.fit(housing_prepared, housing_labels)
-    cv_results(rnd_search)
+        lin_reg = LinearRegression()
+        lin_reg.fit(housing_prepared, housing_labels)
 
-    param_grid = [
-        {"n_estimators": [3, 10, 30], "max_features": [2, 4, 6, 8]},
-        {"bootstrap": [False], "n_estimators": [3, 10], "max_features": [2, 3, 4]},
-    ]
-    forest_reg = RandomForestRegressor(random_state=42)
+        tree_reg = DecisionTreeRegressor(random_state=42)
+        tree_reg.fit(housing_prepared, housing_labels)
 
-    grid_search = GridSearchCV(
-        forest_reg,
-        param_grid,
-        cv=5,
-        scoring="neg_mean_squared_error",
-        return_train_score=True,
-    )
-    grid_search.fit(housing_prepared, housing_labels)
-    cv_results(grid_search)
+        param_distribs = {
+            "n_estimators": randint(low=1, high=200),
+            "max_features": randint(low=1, high=8),
+        }
+        forest_reg = RandomForestRegressor(random_state=42)
+        rnd_search = RandomizedSearchCV(
+            forest_reg,
+            param_distributions=param_distribs,
+            n_iter=10,
+            cv=5,
+            scoring="neg_mean_squared_error",
+            random_state=42,
+        )
+        rnd_search.fit(housing_prepared, housing_labels)
+        cv_results(rnd_search)
 
+        param_grid = [
+            {"n_estimators": [3, 10, 30], "max_features": [2, 4, 6, 8]},
+            {"bootstrap": [False], "n_estimators": [3, 10], "max_features": [2, 3, 4]},
+        ]
+        forest_reg = RandomForestRegressor(random_state=42)
+
+        grid_search = GridSearchCV(
+            forest_reg,
+            param_grid,
+            cv=5,
+            scoring="neg_mean_squared_error",
+            return_train_score=True,
+        )
+        grid_search.fit(housing_prepared, housing_labels)
+        cv_results(grid_search)
+
+        logger.debug("Model training is completed successfully.")
+    except Exception as e:
+        logger.error(f"Error while model training process: {e}")
     return lin_reg, tree_reg, rnd_search, grid_search
 
 
 def get_best_model_gridsearch(grid_search, housing_prepared):
-    feature_importances = grid_search.best_estimator_.feature_importances_
-    sorted(zip(feature_importances, housing_prepared.columns), reverse=True)
+    try:
+        logger.info("Finging the best models...")
+        feature_importances = grid_search.best_estimator_.feature_importances_
+        sorted(zip(feature_importances, housing_prepared.columns), reverse=True)
+        logger.debug("Best model is selected successfully.")
+    except Exception as e:
+        logger.error(f"Error while best model selection process: {e}")
     return grid_search.best_estimator_
